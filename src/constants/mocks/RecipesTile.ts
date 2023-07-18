cfdfd4463cc286b7a67530b317760f1d2d1502e1
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-07-18 07:40:19 +0200

    refactor: move mock to separate file

---
+++ src/constants/mocks/RecipesTile.ts
@@ -0,0 +1,28 @@
+import { RecipeTile } from "../../types/RecipeTypes";
+
+export const RECIPES_INFO: RecipeTile[] = [
+    {
+      title: "Pancakes",
+      time: "50 min",
+      image:
+        "https://wszystkiegoslodkiego.pl/storage/images/202111/puszyste-pancakes.jpg",
+    },
+    {
+      title: "Pancakes 2",
+      time: "20 min",
+      image:
+        "https://img.delicious.com.au/yXfWTc5v/del/2023/02/p106-pancakes-with-roasted-vanilla-bean-berries-and-nectarines-183881-1.png",
+    },
+    {
+      title: "Sweet Pancakes",
+      time: "2 hour",
+      image:
+        "https://www.pogotujmy.pl/wp-content/uploads/2015/10/PANCAKES-2.jpg?p=131",
+    },
+    {
+      title: "Sweet Pancakes",
+      time: "2 hour",
+      image:
+        "https://www.pogotujmy.pl/wp-content/uploads/2015/10/PANCAKES-2.jpg?p=131",
+    },
+  ];
