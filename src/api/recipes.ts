e9e0b6cf035c809bc484c799455f011f1d2e5d6d
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-07-19 13:11:24 +0200

    refactor: global create collection function

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -1,19 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import { CollectionReference, DocumentData, collection, getDocs} from "firebase/firestore";
-import { db } from "../firebase";
+import { DocumentData, getDocs } from "firebase/firestore";
 import { Recipe } from "../types/RecipeTypes";
 import { FirebaseError } from "firebase/app";
 import { QueryKeys } from "../enums/QueryKeys";
 import { Collections } from "../enums/Collections";
+import { createCollection } from "../utils/recipeUtils";
 
 export function useFetchRecipes() {
   return useQuery<Recipe[], FirebaseError>({
     queryKey: [QueryKeys.recipesData],
     queryFn: async () => {
-      const recipeCollection = collection(db, Collections.recipes) as CollectionReference<Recipe>
-      const recipeSnap = await getDocs(recipeCollection)
-      const recipes = recipeSnap.docs.map((item : DocumentData) => item.data())
-      return recipes
+      const recipeCollection = createCollection<Recipe>(Collections.recipes);
+      const recipeSnap = await getDocs(recipeCollection);
+      const recipes = recipeSnap.docs.map((item: DocumentData) => item.data());
+      return recipes;
     },
-  })
+  });
 }
