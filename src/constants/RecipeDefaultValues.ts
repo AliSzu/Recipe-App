514ce205fde74c57f280d9edd46afeedd7ff5cc0
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-08-07 15:13:39 +0200

    refactor: default image

--- src/constants/RecipeDefaultValues.ts
+++ src/constants/RecipeDefaultValues.ts
@@ -1,12 +1,14 @@
 import { RecipeFormValues } from "../types/FormTypes";
 import { uniqueId } from "../utils/recipeUtils";
 
+export const DEFAULT_IMAGE =
+  "https://firebasestorage.googleapis.com/v0/b/recipe-app-c8936.appspot.com/o/recipe%2FdefaultDish.jpeg?alt=media&token=724b6767-874e-42e6-aab5-3af18cb8b1af";
 
 export const recipeDefaultValues: RecipeFormValues = {
   title: "",
   time: "",
   description: "",
-  imgSrc: import.meta.env.VITE_DEFAULT_DISH_URL,
+  imgSrc: DEFAULT_IMAGE,
   ingredients: [
     {
       amount: "",
