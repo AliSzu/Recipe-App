609fd5e820c20b3b6fa3fe488712405be51c94d7
Alicja Szulecka <aliszu1@st.amu.edu.pl>
2023-08-23 12:15:51 +0200

    test: navbar icon

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,20 @@
+import { render, fireEvent } from "@testing-library/react";
+import { expect, test } from "vitest";
+import NavbarIcon from "../components/atoms/NavbarIcon";
+import { MemoryRouter } from "react-router-dom";
+import { ROUTES } from "../constants/Routes";
+
+test("navbar icon navigates to homepage", async () => {
+    const { container } = render(
+        <MemoryRouter initialEntries={[ROUTES.ADD_RECIPE]}>
+          <NavbarIcon />
+        </MemoryRouter>
+      );
+
+  const navbarIcon = container.firstChild;
+  fireEvent.click(navbarIcon!);
+
+  expect(window.location.pathname).toBe(ROUTES.HOME);
+});
+
+
