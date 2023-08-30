64181e2fbc3ba00cb7c1cf3848fae49559c1ed0d
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-08-30 10:20:06 +0200

    Update RootContainer.tsx

--- src/router/RootContainer.tsx
+++ src/router/RootContainer.tsx
@@ -14,9 +14,9 @@ const RootContainer = () => {
         <AnimatePresence mode="popLayout">
           <motion.div
             key={location.pathname}
-            initial={{ opacity: 0, x: 50 }}
+            initial={{ opacity: 0, x: 10 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -50 }}
+            exit={{ opacity: 0, x: -10 }}
             transition={{ duration: 0.3 }}
           >
             <AnimatedOutlet />
